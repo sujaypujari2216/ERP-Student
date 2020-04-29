ed49c12265a6ded2a9f4b0b886dc8c03e6f37308
Sujay <sujaypujari2216@gmail.com>
2020-04-29 17:03:12 +0530

    side-bar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'my-profile', component:MyProfileComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
