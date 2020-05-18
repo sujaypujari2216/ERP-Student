6b4922f1ea48618656bd9fd3557d1856d175346b
Sujay <sujaypujari2216@gmail.com>
2020-05-18 10:56:52 +0530

    loader added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { PreloaderService } from './shared/preloader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
+  constructor(private preloader: PreloaderService) {}
   title = 'ERP-Student';
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.preloader.hide();
+  }
 }
